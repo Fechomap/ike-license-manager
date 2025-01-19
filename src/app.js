49789cbac04a29f9f46739e54c4664f834618669
Fechomap <jhon_free27@hotmail.com>
2025-01-19 14:02:19 -0600

    Deploy to Heroku

--- src/app.js
+++ src/app.js
@@ -6,7 +6,11 @@ const apiRoutes = require('./routes/apiRoutes');
 const telegramService = require('./services/telegramService'); // Cambia esto
 
 const app = express();
-const PORT = process.env.PORT || 3000;
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 // Conectar a MongoDB
 connectDB();
